b1fe52179f2826a14b611bf82fa588275ac43e5c
Jagienka <cwiekowska.agnieszka@gmail.com>
2021-02-18 20:42:47 +0100

    design view and add event_id list

--- kalendarz.py
+++ kalendarz.py
@@ -5,12 +5,17 @@ import datetime
 
 from db import Database
 
+event_id = []
 
 def grab_data():
-	query = "INSERT INTO events (date, stop_event, description) VALUES(%s, %s, %s)"
+	query = "INSERT INTO events (date, start_event, stop_event, description) VALUES(%s, %s, %s, %s)"
 	date= cal.selection_get().strftime("%Y-%m-%d")
 	db.connect()
-	db.insert(query, (date, stop_hour.get(), description.get()))
+	field_1 = date
+	field_2 = start_hour.get()
+	field_3 = stop_hour.get()
+	field_4 = description.get()
+	db.insert(query, (field_1, field_2, field_3, field_4))
 	db.close()
 
 	get_data_from_table()
@@ -18,46 +23,72 @@ def grab_data():
 def get_data_from_table():
 	list_of_event.delete(0, 100)
 	date = cal.selection_get().strftime("%Y-%m-%d")
-	query = "SELECT description FROM events where date='"+date+"'"
+	query = "SELECT start_event, stop_event, description, id FROM events where date='"+date+"' ORDER BY start_event"
 	db.connect()
 	i = 0
 	for x in db.execute_query(query, ""):
-		list_of_event.insert(i, x[0])
+		field_1 = str(x[0])
+		field_2 = str(x[1])
+		field_3 = str(x[2])
+		event_id.insert(i, x[3]) 
+		list_of_event.insert(i, field_1+" | "+field_2+ " | "+ field_3)
 		i=i+1
+	if i == 0:
+		list_of_event.insert(i, "BRAK WYDARZEŃ")
 	db.close()
 
 
 def change_date(event):
-	my_label.config(text=cal.selection_get())
+	date_label.config(text=cal.selection_get())
 	get_data_from_table()
+
+
+def change_listbox(event):
+	selection = event.widget.curselection()[0]
+	event_id_str = str(event_id[selection])
+	print(selection)
+	query = "SELECT start_event, stop_event, description FROM events where id='"+event_id_str+"'"
+	db.connect()
+	for x in db.execute_query(query, ""):
+		print(x)
+	db.close()
+
 
 
 if __name__ == "__main__":
 	today = datetime.datetime.now()
 
 	root = Tk() 
-	root.geometry("600x400")
+	root.geometry("700x380")
+	root.title("PLANER")
 
 	cal = Calendar(root, selectmode="day", year=today.year, month=today.month, day=today.day)
-	my_button = Button(root, text="Insert event", command=grab_data)
+	my_button = Button(root, text="Dodaj wydarzenie", command=grab_data)
 	list_of_event = Listbox(root, height=10, width=40)
 	start_hour = Entry(root)
 	stop_hour = Entry(root)
-	description = Entry(root)
-	my_label = Label(root, text="")
+	description = Entry(root, width=40)
+	date_label = Label(root, text="")
+	start_hour_label = Label(root, text="Godzina początek")
+	stop_hour_label = Label(root, text="Godzina koniec")
+	description_label = Label(root, text="Opis wydarzenia")
+	list_of_event_label = Label(root, text="Twoje wydarzenia w ten dzień")
 
-	cal.grid(row=0, column=0)
-	my_button.grid(row=3, column=0)
-	start_hour.grid(row=5, column=0)
-	stop_hour.grid(row=7, column=0)
-	description.grid(row=9, column=0)
-	my_label.grid(row=11, column=0)
+	cal.grid(row=0, column=0, rowspan=8)
+	start_hour_label.grid(row=8, column=0)
+	start_hour.grid(row=9, column=0)
+	stop_hour_label.grid(row=10, column=0)
+	stop_hour.grid(row=11, column=0)
+	description_label.grid(row=12, column=0)
+	description.grid(row=13, column=0)
+	date_label.grid(row=14, column=0)
+	my_button.grid(row=15, column=0)
 
-	list_of_event.grid(row=0, column=1, columnspan=3)
-
-
+	list_of_event_label.grid(row=0, column=4)
+	list_of_event.grid(row=1, column=4, rowspan=10)
 
 	cal.bind("<<CalendarSelected>>", change_date)
+	list_of_event.bind("<<ListboxSelect>>", change_listbox)
 
 
 
@@ -69,30 +100,8 @@ if __name__ == "__main__":
 	db.create_table(sql_as_string)
 	db.close()
 
-	my_label.config(text=cal.selection_get())
+	date_label.config(text=cal.selection_get())
 	get_data_from_table()
 
 
-
-
-
-  
-# root.geometry("600x400") 
-  
-# cal = Calendar(root, selectmode = 'day', 
-#                year = 2020, month = 5, 
-#                day = 22) 
-  
-# cal.pack(pady = 20) 
-  
-# def grad_date(): 
-#     date.config(text = "Selected Date is: " + cal.get_date()) 
-  
-# Button(root, text = "Get Date", 
-#        command = grad_date).pack(pady = 20) 
-  
-# date = Label(root, text = "") 
-# date.pack(pady = 20) 
-  
-
-root.mainloop()
+	root.mainloop()
